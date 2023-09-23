a827fb3e79c9ea0be2ae94f8ccbe106e750c54aa
hellozhouwenjie <133631960+hellozhouwenjie@users.noreply.github.com>
2023-09-23 18:47:40 +0800

    Update 1-3打印文本值.cs

--- 1-3打印文本值.cs
+++ 1-3打印文本值.cs
@@ -1,2 +1,13 @@
-12345678
-Console.WriteLine('b');Console.WriteLine(123);Console.WriteLine(0.25F);Console.WriteLine(0.25);Console.WriteLine(12.39816m);Console.WriteLine(true);Console.WriteLine(false);
+Console.WriteLine('b');
+
+Console.WriteLine(123);
+
+Console.WriteLine(0.25F);
+
+Console.WriteLine(0.25);
+
+Console.WriteLine(12.39816m);
+
+Console.WriteLine(true);
+
+Console.WriteLine(false);
