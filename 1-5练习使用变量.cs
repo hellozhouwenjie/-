66896d314b4cd284b92668ff5aebeac41f738242
hellozhouwenjie <133631960+hellozhouwenjie@users.noreply.github.com>
2023-09-23 19:11:04 +0800

    Create 1-5练习使用变量.cs

---
+++ 1-5练习使用变量.cs
@@ -0,0 +1,17 @@
+string firstName;
+
+firstName = "Bob";
+
+Console.WriteLine(firstName);
+
+firstName = "Liem";
+
+Console.WriteLine(firstName);
+
+firstName = "Isabella";
+
+Console.WriteLine(firstName);
+
+firstName = "Yasmin";
+
+Console.WriteLine(firstName);
