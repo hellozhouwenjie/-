874b33ba93769571c5fd62708afd877b3f135ad4
hellozhouwenjie <133631960+hellozhouwenjie@users.noreply.github.com>
2023-09-23 19:35:57 +0800

    Create 1-7挑战显示文本和变量值.cs

---
+++ 1-7挑战显示文本和变量值.cs
@@ -0,0 +1,14 @@
+
+string firstName;
+
+int message;
+
+float temperature;
+
+firstName="Bob";
+
+message=3;
+
+temperature=34.4f;
+
+Console.WriteLine("Hello,"+firstName+"! You have "+message+" messages in your inbox. the temperature is "+temperature+" celsius.");
